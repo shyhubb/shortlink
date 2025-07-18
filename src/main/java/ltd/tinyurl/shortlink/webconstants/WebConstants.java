ffd25c6b272c57a15d101f551d0fba49f5468457
shyhubb <shyhubel@gmai.com>
2025-07-18 19:39:00 +0700

    new deloy

--- src/main/java/ltd/tinyurl/shortlink/webconstants/WebConstants.java
+++ src/main/java/ltd/tinyurl/shortlink/webconstants/WebConstants.java
@@ -10,7 +10,7 @@ public class WebConstants {
     public static final String PRIVATE_SHORT_LINK_API_V1_PATH = API_VERSION_V1 + "/user/link";
     public static final String AUTH_API_V1_PATH = API_VERSION_V1 + "/auth";
     public static final String USER_API_V1_PATH = API_VERSION_V1 + "/user";
-    public static final String BASE_SHORT_URL_DOMAIN = "https://link3s.onrender.com/";
+    public static final String BASE_SHORT_URL_DOMAIN = "https://linkz.onrender.com";
     public static final String URL_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     public static final int LENGTH_KEY_URL = 6;
     public static final int MAX_ATTEMPTS_GENERATE_KEY = 10;
